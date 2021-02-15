68d482a16a3147c4db11ceb720ccd85ab84bcbcb
Noel Andersson <noel.anderssonkohler@hotmail.com>
2021-02-15 19:12:18 +0100

    First commit

---
+++ precache-manifest.cc0b2ead381ede75338a30396e11b06a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757717c928c367cfadfc",
+    "url": "/css/app.6703ad92.css"
+  },
+  {
+    "revision": "b8ef1dc4ceac423ccb2c",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "b5e1f85dc6b950b33675c55aee669cff",
+    "url": "/img/Icon.b5e1f85d.svg"
+  },
+  {
+    "revision": "56831e1b4932f14f5431a0145a51ff63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "757717c928c367cfadfc",
+    "url": "/js/app.336ab451.js"
+  },
+  {
+    "revision": "b8ef1dc4ceac423ccb2c",
+    "url": "/js/chunk-vendors.757010b5.js"
+  },
+  {
+    "revision": "08b5710eabd5b1a8df54469adc16aadf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
